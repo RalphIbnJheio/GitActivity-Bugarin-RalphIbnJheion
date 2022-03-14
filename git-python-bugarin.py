e2b8b7ed2718d181c19041ac9782f7804fae5b13
BugarinRalph <dyj12.oryxa@gmail.com>
2022-03-14 18:01:42 +0800

    MyHobbiesAdded

--- git-python-bugarin.py
+++ git-python-bugarin.py
@@ -4,3 +4,12 @@ print ("Ihatub, Boac, Marinduque")
 
 print ("November 15, 2001")
 
+print ("Cycling")
+
+print ("Volleyball")
+
+print ("Mag Inom")
+
+print ("going to the Gym")
+
+print ("Sleeping")
